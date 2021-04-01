bbab4c3e8b003b8f442328f70ae9db11629eb5ac
LoganHaug <logannhaug@gmail.com>
2021-03-31 21:39:56 -0700

    Add image reading and name matching

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # attendance-ocr
 A python script which takes attendance using OCR
+## Install the script
+- Run `pip install virualenv` and run `python3 venv .venv` to create the virtual environment
+- Run `pip install -r requirements.txt` to install project dependencies
+- Run `source .venv/bin/activate` to start the virtual environment
---
+++ main.py
@@ -0,0 +1,20 @@
+import re
+import csv
+
+from PIL import Image
+import pytesseract
+
+ 
+# Simple image to string
+IMAGE_STR = pytesseract.image_to_string(Image.open("test.png"))
+IMAGE_STR = IMAGE_STR.lower()
+names = []
+
+with open("names.csv", "r") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        names.append((row[0].lower(), row[1].lower()))
+
+for name in names:
+    if re.search(f"{name[0]} {name[1]}", IMAGE_STR) is None:
+        print(f"Didn't find {name[0]} {name[1]}")
